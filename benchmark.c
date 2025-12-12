a64241080847991b9f909be934333b26067712d8
Jack Stow <getjackstow@gmail.com>
2025-12-12 00:36:38 +0000

    removed the array tracking all malloc pointers. it was contaminating results and using tremendous memory. Also got sick of recompiling with new NUM_BLOCKS values, so I added arguments.

--- benchmark.c
+++ benchmark.c
@@ -4,15 +4,25 @@
 #include <time.h>
 #include "arena.h"
 
-#define NUM_BLOCKS ((size_t)5000000ULL)
-#define BLOCK_SIZE ((size_t)4)
+//#define NUM_BLOCKS ((size_t)10000000ULL)
+//#define BLOCK_SIZE ((size_t)4)
 
 // Helper: compute elapsed time in seconds from timespec
 double timespec_diff_sec(struct timespec start, struct timespec end) {
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 }
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		fprintf(stderr, "Usage %s <num_blocks> <block_size>\n", argv[0]);
+		return 1;
+	}
+
+	size_t NUM_BLOCKS = strtoull(argv[1], NULL, 10);
+	size_t BLOCK_SIZE = strtoull(argv[2], NULL, 10);
+
+	printf("Running benchmark with %zu blocks, block size %zu bytes \n", NUM_BLOCKS, BLOCK_SIZE);
+
     // ----------------------
     // Benchmark malloc/free
     // ----------------------
@@ -20,24 +30,25 @@ int main() {
     clock_t cstart, cend;
     double malloc_cpu_time, malloc_wall_time;
 
-    void **blocks = malloc(NUM_BLOCKS * sizeof(void *));
-    if (!blocks){
-		printf("Failed to allocate blocks array\n");
-		return 1;
-    }
+    //void **blocks = malloc(NUM_BLOCKS * sizeof(void *));
+    //if (!blocks){
+	//	printf("Failed to allocate blocks array\n");
+	//	return 1;
+    //}
 
     clock_gettime(CLOCK_MONOTONIC, &wstart);
     cstart = clock();
 
     for (size_t i = 0; i < NUM_BLOCKS; i++) {
-        blocks[i] = malloc(BLOCK_SIZE);
-        if (!blocks[i]) {
+        void *ptr = malloc(BLOCK_SIZE);
+        if (!ptr) {
             printf("malloc failed at %zu\n", i);
             return 1;
         }
-        memset(blocks[i], 0xAB, BLOCK_SIZE);
+        memset(ptr, 0xAB, BLOCK_SIZE);
+        free(ptr);
     }
-    for (size_t i = 0; i < NUM_BLOCKS; i++) free(blocks[i]);
+    //for (size_t i = 0; i < NUM_BLOCKS; i++) free(blocks[i]);
 
     cend = clock();
     clock_gettime(CLOCK_MONOTONIC, &wend);
